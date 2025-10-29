08fa2638bbf9bbf1ac4f4536344b00d0a642de17
Santiago Pedraza <santiagopedrazat5@gmail.com>
2025-10-29 17:38:58 -0300

    Prueba1

--- IngresosyGastosMensuales/settings.py
+++ IngresosyGastosMensuales/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o^np%ds0th5kjt*ad0@$m*8hcjp-5givfeg^24k7j=z&&kx&5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
